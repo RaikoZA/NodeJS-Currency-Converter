5e2dacd6a00facfd395a390bd3c6f23ce83d7a44
RaikoZA <donavan691@gmail.com>
2017-02-01 14:27:19 +0200

    Update currencyConverter.js

--- currencyConverter.js
+++ currencyConverter.js
@@ -1,66 +1,57 @@
 var request = require('request');
 var cheerio = require('cheerio');
 
-var roundOff = function(currency){
+/**
+* Rounds off currency output 2 decimal places
+*/
 
-	/**
-	* Rounds off currency output 2 decimal places
-	*/
+var roundOff = function (currency) {
 
-	return Math.round(currency * 100) / 100;
-
-}
-
-var getCurrency = function(param, param1){
-
-	/**
-	* Converts two parameters from param to param1 eg: USD to ZAR
-	* @param string selects the currency you would like to convert
-	* @param2 string converts the first param to new currency user is requesting
-	*
-	*/
-
-	var url = 'http://themoneyconverter.com/'+param+'/'+param1+'.aspx'
-
-	return sendRequest(url);	
+  return Math.round(currency * 100) / 100;
 
 }
 
-var sendRequest = function(currencyUrl){
+/**
+* Converts two parameters from param to param1 eg: USD to ZAR
+* @param fromCurrency selects the currency you would like to convert
+* @param toCurrency converts fromCurrency to new currency user is requesting
+*/
 
-	/**
-	* Sends request to currency url with user params
-	* @currencyUrl string returns the function getCurrency url
-	*/
+var getCurrency = function (fromCurrency, toCurrency) {
 
-	request(
-		{
-			url: currencyUrl
-		}, 
+  var url = 'http://themoneyconverter.com/'+fromCurrency+'/'+toCurrency+'.aspx'
 
-		function(err, response, body){
-			if(err){
-				console.log('Error: ', err);
-			}
+  return sendRequest(url);	
 
-			var $ = cheerio.load(body);
-			var currency = $('div.tmc-well.switch-table > p > b').text();
-
-			console.log(roundOff(currency));
-
-		})
 }
 
-var returnResult = function(param, param1){
+/**
+* Sends request to currency url with user params
+* @currencyUrl string returns the function getCurrency url
+*/
 
-	getCurrency(param, param1, function(err, result){
-		if(err){
-			console.log('Error:', err)
-		}else{
-			return result;
-		}
-	})
+var sendRequest = function (currencyUrl) {
+
+  request({ url: currencyUrl }, function (err, response, body) {
+    if (err) {
+      console.log('Error: ', err);
+    }
+
+    var $ = cheerio.load(body);
+    var currency = $('div.tmc-well.switch-table > p > b').text();
+	  
+    console.log(roundOff(currency));
+
+    })
 }
 
+var returnResult = function (fromCurrency, toCurrency) {
 
-//returnResult('usd', 'zar');
+  getCurrency(fromCurrency, toCurrency, function (err, result) {
+    if (err) {
+      console.log('Error:', err)
+    }else {
+      return result;
+    }
+  })
+}
